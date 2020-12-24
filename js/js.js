be2f9a3854c18543d9d1a1d6bddeb9fa9b9c18a6
anderama <brunnowtf@gmail.com>
2020-12-23 23:16:15 -0300

    Adicionando movimentação ao Helicóptero

--- js/js.js
+++ js/js.js
@@ -10,6 +10,24 @@ function start() { // Inicio da função start()
 	//Principais variáveis do jogo
 	
 	var jogo = {}
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+		}
+	
+		jogo.pressionou = [];
+
+		//Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+		jogo.pressionou[e.which] = true;
+		});
+	
+	
+		$(document).keyup(function(e){
+		   jogo.pressionou[e.which] = false;
+		});
 	
 	//Game Loop
 
@@ -18,6 +36,7 @@ function start() { // Inicio da função start()
 	function loop() {
 	
 	movefundo();
+	movejogador();
 	
 	} // Fim da função loop()
 
@@ -29,5 +48,47 @@ function start() { // Inicio da função start()
 		$("#fundoGame").css("background-position",esquerda-1);
 		
 		} // fim da função movefundo()
+
+	function movejogador() {
+	
+			if (jogo.pressionou[TECLA.W]) {
+				var topo = parseInt($("#jogador").css("top"));
+				$("#jogador").css("top",topo-10);
+			
+			}
+			
+			if (jogo.pressionou[TECLA.S]) {
+				
+				var topo = parseInt($("#jogador").css("top"));
+				$("#jogador").css("top",topo+10);	
+			}
+			
+			if (jogo.pressionou[TECLA.D]) {
+				
+				//Chama função Disparo	
+			}
+		
+			} // fim da função movejogador()
+
+	function movejogador() {
+	
+	if (jogo.pressionou[TECLA.W]) {
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo-10);
+	
+	}
+	
+	if (jogo.pressionou[TECLA.S]) {
+		
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo+10);	
+	}
+	
+	if (jogo.pressionou[TECLA.D]) {
+		
+		//Chama função Disparo	
+	}
+
+	} // fim da função movejogador()
 
 } // Fim da função start
