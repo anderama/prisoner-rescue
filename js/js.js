c655a392aa0031e95790b2fa578366fd84c0ced0
anderama <brunnowtf@gmail.com>
2020-12-23 23:45:31 -0300

    Adicionando a função para mover o inimigo 2

--- js/js.js
+++ js/js.js
@@ -40,6 +40,8 @@ function start() { // Inicio da função start()
 	movefundo();
 	movejogador();
 	moveinimigo1();
+	moveinimigo2();
+	moveamigo();
 	
 	} // Fim da função loop()
 
@@ -116,5 +118,29 @@ function start() { // Inicio da função start()
 				
 			}
 	} //Fim da função moveinimigo1()
+
+	function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+	$("#inimigo2").css("left",posicaoX-3);
+				
+		if (posicaoX<=0) {
+			
+		$("#inimigo2").css("left",775);
+					
+		}
+} // Fim da função moveinimigo2()
+
+function moveamigo() {
+	
+	posicaoX = parseInt($("#amigo").css("left"));
+	$("#amigo").css("left",posicaoX+1);
+				
+		if (posicaoX>906) {
+			
+		$("#amigo").css("left",0);
+					
+		}
+
+} // fim da função moveamigo()
 
 } // Fim da função start
