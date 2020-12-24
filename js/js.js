56f4a3267c3a80be925debb88b517ef96661a02c
anderama <brunnowtf@gmail.com>
2020-12-23 23:36:19 -0300

    Adicionando movimentação do inimigo

--- js/js.js
+++ js/js.js
@@ -10,6 +10,8 @@ function start() { // Inicio da função start()
 	//Principais variáveis do jogo
 	
 	var jogo = {}
+	var velocidade=5;
+	var posicaoY = parseInt(Math.random() * 334);
 	var TECLA = {
 		W: 87,
 		S: 83,
@@ -37,6 +39,7 @@ function start() { // Inicio da função start()
 	
 	movefundo();
 	movejogador();
+	moveinimigo1();
 	
 	} // Fim da função loop()
 
@@ -55,12 +58,21 @@ function start() { // Inicio da função start()
 				var topo = parseInt($("#jogador").css("top"));
 				$("#jogador").css("top",topo-10);
 			
+					if (topo <= 0 ) {
+			
+						$("#jogador").css("top",topo+10);
+					}
 			}
 			
 			if (jogo.pressionou[TECLA.S]) {
 				
 				var topo = parseInt($("#jogador").css("top"));
 				$("#jogador").css("top",topo+10);	
+
+					if (topo>=434) {	
+						$("#jogador").css("top",topo-10);
+							
+					}
 			}
 			
 			if (jogo.pressionou[TECLA.D]) {
@@ -90,5 +102,19 @@ function start() { // Inicio da função start()
 	}
 
 	} // fim da função movejogador()
+
+	function moveinimigo1() {
+
+		posicaoX = parseInt($("#inimigo1").css("left"));
+		$("#inimigo1").css("left",posicaoX-velocidade);
+		$("#inimigo1").css("top",posicaoY);
+			
+			if (posicaoX<=0) {
+			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left",694);
+			$("#inimigo1").css("top",posicaoY);
+				
+			}
+	} //Fim da função moveinimigo1()
 
 } // Fim da função start
