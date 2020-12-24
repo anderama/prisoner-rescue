82118e1352adcb4adff35d0a6bffdbc063a17bbd
anderama <brunnowtf@gmail.com>
2020-12-23 22:53:59 -0300

    Criando a função que movimenta o fundo do jogo

--- js/js.js
+++ js/js.js
@@ -7,4 +7,27 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+	//Principais variáveis do jogo
+	
+	var jogo = {}
+	
+	//Game Loop
+
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	
+	} // Fim da função loop()
+
+	//Função que movimenta o fundo do jogo
+	
+	function movefundo() {
+	
+		esquerda = parseInt($("#fundoGame").css("background-position"));
+		$("#fundoGame").css("background-position",esquerda-1);
+		
+		} // fim da função movefundo()
+
 } // Fim da função start
