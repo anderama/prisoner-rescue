edc0f94076b22eb18d9bf666550643f30da96fd7
anderama <brunnowtf@gmail.com>
2020-12-24 00:52:50 -0300

    Adicionando função de energia

--- js/js.js
+++ js/js.js
@@ -7,6 +7,7 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 	$("#fundoGame").append("<div id='placar'></div>");
+	$("#fundoGame").append("<div id='energia'></div>");
 
 	//Principais variáveis do jogo
 	
@@ -16,6 +17,7 @@ function start() { // Inicio da função start()
 	var pontos=0;
 	var salvos=0;
 	var perdidos=0;
+	var energiaAtual=3;
 	var velocidade = 5;
 	var posicaoY = parseInt(Math.random() * 334);
 	var TECLA = {
@@ -50,6 +52,7 @@ function start() { // Inicio da função start()
 	moveamigo();
 	colisao();
 	placar();
+	energia();
 	
 	} // Fim da função loop()
 
@@ -197,6 +200,7 @@ function colisao() {
 		
 		if (colisao1.length>0) {
 			
+		energiaAtual--;
 		inimigo1X = parseInt($("#inimigo1").css("left"));
 		inimigo1Y = parseInt($("#inimigo1").css("top"));
 		explosao1(inimigo1X,inimigo1Y);
@@ -208,7 +212,8 @@ function colisao() {
 
 		// jogador com o inimigo2 
 		if (colisao2.length>0) {
-	
+			
+			energiaAtual--;
 			inimigo2X = parseInt($("#inimigo2").css("left"));
 			inimigo2Y = parseInt($("#inimigo2").css("top"));
 			explosao2(inimigo2X,inimigo2Y);
@@ -378,5 +383,33 @@ function explosao2(inimigo2X,inimigo2Y) {
 		$("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
 		
 	} //fim da função placar()		
+
+	//Barra de energia
+
+function energia() {
+	
+	if (energiaAtual==3) {
+		
+		$("#energia").css("background-image", "url(imgs/energia3.png)");
+	}
+
+	if (energiaAtual==2) {
+		
+		$("#energia").css("background-image", "url(imgs/energia2.png)");
+	}
+
+	if (energiaAtual==1) {
+		
+		$("#energia").css("background-image", "url(imgs/energia1.png)");
+	}
+
+	if (energiaAtual==0) {
+		
+		$("#energia").css("background-image", "url(imgs/energia0.png)");
+		
+		//Game Over
+	}
+
+} // Fim da função energia()
 
 } // Fim da função start
