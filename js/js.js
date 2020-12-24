f00e2843eb26e3dc28185ef0bfd581bb182f48c7
anderama <brunnowtf@gmail.com>
2020-12-24 00:06:56 -0300

    Adicionando a função de explosão

--- js/js.js
+++ js/js.js
@@ -183,9 +183,38 @@ function disparo() {
 function colisao() {
 	var colisao1 = ($("#jogador").collision($("#inimigo1")));
 	// jogador com o inimigo1
+		
+		if (colisao1.length>0) {
+			
+		inimigo1X = parseInt($("#inimigo1").css("left"));
+		inimigo1Y = parseInt($("#inimigo1").css("top"));
+		explosao1(inimigo1X,inimigo1Y);
+	
+		posicaoY = parseInt(Math.random() * 334);
+		$("#inimigo1").css("left",694);
+		$("#inimigo1").css("top",posicaoY);
+		}
+	
+	} //Fim da função colisao()
 
-	console.log(colisao1);
-
-} //Fim da função colisao()
+function explosao1(inimigo1X,inimigo1Y) {
+	$("#fundoGame").append("<div id='explosao1'></div");
+	$("#explosao1").css("background-image", "url(imgs/explosao.png)");
+	var div=$("#explosao1");
+	div.css("top", inimigo1Y);
+	div.css("left", inimigo1X);
+	div.animate({width:200, opacity:0}, "slow");
+	
+	var tempoExplosao=window.setInterval(removeExplosao, 1000);
+	
+		function removeExplosao() {
+			
+			div.remove();
+			window.clearInterval(tempoExplosao);
+			tempoExplosao=null;
+			
+		}
+		
+	} // Fim da função explosao1()
 
 } // Fim da função start
